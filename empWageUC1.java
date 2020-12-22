2149d096a7f2a05df958e06547d32904cd87b450
paraglande03 <paraglande03@gmail.com>
2020-12-22 20:45:10 +0530

    ADD Emp Attendance

---
+++ empWageUC1.java
@@ -0,0 +1,21 @@
+public class empWageUC1
+{
+ 	public static void main(String[] args)
+	{
+		int Result=(int)(Math.random()*10)%2;
+		int isPresent=0;
+	
+		if(Result == isPresent)
+		{
+			System.out.println("Employee is present");
+	
+	        }
+
+	 	else 
+		{
+			System.out.println("Employee is Absent");
+		}           
+		
+
+	}
+}
